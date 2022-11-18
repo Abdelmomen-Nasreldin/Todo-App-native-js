2ac09c8a968fcb2a0903e4d4c90ef30375fafda3
Hady  Wafa <62218928+hadywafa@users.noreply.github.com>
2022-11-18 14:40:47 +0200

    fix some feature

--- todo.js
+++ todo.js
@@ -5,84 +5,66 @@ let todoList = todoContainer.querySelector("#todoList");
 let firstTodoItem = todoList.querySelector(".todo--item");
 let doneItemsDom = document.querySelector("#todoDoneItems");
 
-let addTodoHandler = () => { // the short way  
-    let copyTodoItem = firstTodoItem.cloneNode(true);
-    copyTodoItem.firstElementChild.innerText = addTodoInput.value;
-    // copyTodoItem.id = `${idGenerator++}`;
-    todoList.prepend(copyTodoItem);
+let addTodoHandler = () => {
+  // the short way
+  let copyTodoItem = firstTodoItem.cloneNode(true);
+  copyTodoItem.firstElementChild.innerText = addTodoInput.value;
+  // copyTodoItem.id = `${idGenerator++}`;
+  todoList.prepend(copyTodoItem);
 };
 //////////////////////////
 let idGenerator = 1;
 // const todoItemsArr = [];
-let createElementTemplateHandler = (
-    elementType = "div",
-    elementInnerHtml = "",
-    elementClassName = ""
-) => {
-    let todoItemType = document.createElement(elementType);
-    todoItemType.innerHTML = elementInnerHtml;
-    todoItemType.classList.add(elementClassName);
-    return todoItemType;
+let createElementTemplateHandler = (elementType = "div", elementInnerHtml = "", elementClassName = "") => {
+  let todoItemType = document.createElement(elementType);
+  todoItemType.innerHTML = elementInnerHtml;
+  todoItemType.classList.add(elementClassName);
+  return todoItemType;
 };
 let checkIfThereIsInputValue = () => {
-    let inputValue = addTodoInput.value
-    if (inputValue < 1) {
-        addTodoInput.classList.add("error");
-        return
-    }
-    addTodoInput.classList.remove("error");
-    return inputValue
-}
+  let inputValue = addTodoInput.value;
+  if (inputValue < 1) {
+    addTodoInput.classList.add("error");
+    return;
+  }
+  addTodoInput.classList.remove("error");
+  return inputValue;
+};
 let createTodoItemDomHandler = (todoText) => {
-    let todoItemText = createElementTemplateHandler(
-        "div",
-        todoText,
-        "todo--item--text"
-    );
-    let todoItemDone = createElementTemplateHandler(
-        "div",
-        '<input type="checkbox" name="todoItemCheckbox" class="todo--item--checkbox">',
-        "todo--item--done"
-    );
-    let todoItemDelete = createElementTemplateHandler(
-        "div",
-        "delete",
-        "todo--item--delete"
-    );
+  let todoItemText = createElementTemplateHandler("div", todoText, "todo--item--text");
+  let todoItemDone = createElementTemplateHandler(
+    "div",
+    '<input type="checkbox" name="todoItemCheckbox" class="todo--item--checkbox">',
+    "todo--item--done"
+  );
+  let todoItemDelete = createElementTemplateHandler("div", "delete", "todo--item--delete");
 
-    let todoItemDom = `  
+  let todoItemDom = `  
         <div class=${todoItemText.className}>
             ${todoItemText.innerHTML}
         </div>
         <div class=${todoItemDone.className}>
             ${todoItemDone.innerHTML}
         </div>
-        <div class=${todoItemDelete.className} onClick=deleteTodoItem(this)>
-        ${todoItemDelete.innerHTML}
-        </div>
-        `
-    return todoItemDom
+        <button class=${todoItemDelete.className} onClick=deleteTodoItem(this)>
+        <i>x</i>
+        </button>
+        `;
+  return todoItemDom;
 };
-
 let addTodoHandlerAlternative = () => {
-    if (!checkIfThereIsInputValue()) {
-        return;
-    }
-    let todoItemDom = createTodoItemDomHandler(addTodoInput.value);
-    let todoItem = createElementTemplateHandler("div", todoItemDom, "todo--item");
-    todoItem.id = `${idGenerator++}`;
-    // let todoItemInnerHtml =  todoItemText + todoItemDone + todoItemDelete; // Not working
-    todoList.prepend(todoItem)
+  if (!checkIfThereIsInputValue()) return;
+  let todoItemDom = createTodoItemDomHandler(addTodoInput.value);
+  let todoItem = createElementTemplateHandler("div", todoItemDom, "todo--item");
+  todoItem.id = `${idGenerator++}`;
+  // let todoItemInnerHtml =  todoItemText + todoItemDone + todoItemDelete; // Not working
+  todoList.prepend(todoItem);
 };
 
-
-function moveToDoneItemsDom(evt) { // more efficient
-    const isChecked = evt.target.checked;
-    console.log("move to done list");
-    if (isChecked) {
-        doneItemsDom.append(evt.target.closest(".todo--item"))
-    }
-}
+// function moveToDoneItemsDom(evt) {
+//   if (evt.target.checked) 
+//     doneItemsDom.append(evt.target.closest(".todo--item"));
+// }
 
 // function moveToDoneItemsDom(){ // bad way
 //     for (let index = 0; index < todoItemsArr.length; index++) {
@@ -93,26 +75,29 @@ function moveToDoneItemsDom(evt) { // more efficient
 //     }
 // }
 
-function moveFromDoneItemsDomToTodoList(evt) {
-    console.log("move to todo list");
-    const isChecked = evt.target.checked;
-    if (isChecked) {
-        return;
-    }
-    todoList.append(evt.target.closest(".todo--item"))
+// function moveFromDoneItemsDomToTodoList(evt) {
+//   if (!evt.target.checked) {
+//     todoList.append(evt.target.closest(".todo--item"));
+//   }
+// }
 
-    
+function updateItemStatue(evt) {
+  if (!evt.target.checked) {
+    todoList.append(evt.target.closest(".todo--item"));
+  }
+  else {
+    doneItemsDom.append(evt.target.closest(".todo--item"));
+  }
 }
 
-function deleteTodoItem(item) { 
-    console.log("delete");
-    item.closest((".todo--item")).remove();
+function deleteTodoItem(item) {
+  console.log("delete");
+  item.closest(".todo--item").remove();
 }
 
 addTodoBtn.addEventListener("click", addTodoHandlerAlternative);
-todoList.addEventListener("change", (event) => moveToDoneItemsDom(event))
-doneItemsDom.addEventListener("change", (event) => moveFromDoneItemsDomToTodoList(event))
-
+todoList.addEventListener("change", (event) => updateItemStatue(event));
+doneItemsDom.addEventListener("change", (event) => updateItemStatue(event));
 
 // addTodoBtn.addEventListener("click", addTodoHandler.bind(this, addTodoInput.value)); // Not working
 // addTodoBtn.addEventListener("click", addTodoHandler); // short way and there is no headache
